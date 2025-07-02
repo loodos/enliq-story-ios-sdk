5a873cdac57eb7cbade36f416d6dcb9b326fc595
FatihTufann <167748615+fatihtufanloodos@users.noreply.github.com>
2025-07-02 16:19:55 +0300

    added Package.swift file

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "EnliqStory",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "EnliqStory",
+            targets: ["EnliqStory"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "EnliqStory",
+            url: "https://github.com/loodos/enliq-story-ios-sdk/releases/download/1.0.0/eiqstory.xcframework.zip",
+            checksum: "5e25d1215bb47eac8bc7aed4a485d15aad8f35b6efeedfc1f475b7d46207decf"
+        ),
+    ]
+)
