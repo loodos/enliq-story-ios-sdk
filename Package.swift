e0778c9baae2511dfedf1399dfed833752e929b9
FatihTufann <167748615+fatihtufanloodos@users.noreply.github.com>
2025-07-04 17:32:53 +0300

    updated Package.swift and sources file

--- Package.swift
+++ Package.swift
@@ -4,29 +4,33 @@ import PackageDescription
 
 let package = Package(
     name: "EIQStory",
+    defaultLocalization: "tr",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v12)
     ],
     products: [
         .library(
             name: "EIQStory",
-            targets: ["EIQStory", "KingfisherReexport"])
+            targets: ["EIQStory", "EIQStorySources"])
     ],
     dependencies: [
-        .package(url: "https://github.com/onevcat/Kingfisher.git", exact: "7.12.0")
+        .package(
+            url: "https://github.com/onevcat/Kingfisher.git",
+            .upToNextMajor(from: "7.0.0")
+        )
     ],
     targets: [
+        .target(
+            name: "EIQStorySources",
+            dependencies: [
+                "Kingfisher"
+            ],
+            path: "Sources"
+        ),
         .binaryTarget(
             name: "EIQStory",
             url: "https://github.com/loodos/enliq-story-ios-sdk/releases/download/1.0.0/eiqstory.xcframework.zip",
             checksum: "5e25d1215bb47eac8bc7aed4a485d15aad8f35b6efeedfc1f475b7d46207decf"
-        ),
-        .target(
-            name: "KingfisherReexport",
-            dependencies: [
-                .product(name: "Kingfisher", package: "Kingfisher")
-            ],
-            path: "Sources"
         )
     ]
 )
